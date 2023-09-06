8b5c0c233851febf2c2e27c56219a884b10b76a1
kyungkwon01 <kyungkwon01@gmail.com>
2023-09-06 16:28:52 -0600

    Added DB logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,40 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB('jate', 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains('jate')) {
+				console.log('jate database already exists');
+				return;
+			}
+			db.createObjectStore('jate', {
+				keyPath: 'id',
+				autoIncrement: true,
+			});
+			console.log('jate database created');
+		},
+	});
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+	const connection = await openDB('jate', 1);
+	const transaction = connection.transaction('jate', 'readwrite');
+	const dataStore = transaction.objectStore('jate');
+	const request = dataStore.add({ jate: content });
+	const result = await request;
+	console.log('Data stored to the database', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Logic for a method that gets all the content from the database
+export const getDb = async () => {
+	console.log('GET all from the database');
+	const connection = await openDB('jate', 1);
+	const transaction = connection.transaction('jate', 'readonly');
+	const dataStore = transaction.objectStore('jate');
+	const request = dataStore.getAll();
+	const result = await request;
+	console.log('result.value', result);
+	return result;
+};
 
 initdb();
