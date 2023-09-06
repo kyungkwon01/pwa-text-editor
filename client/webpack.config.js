aae16d0e1a037de92c95ffe160ebe658cb9aba6e
kyungkwon01 <kyungkwon01@gmail.com>
2023-09-06 16:02:03 -0600

    Added install.js event handlers

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Workbox plugins for a service worker and manifest file.
 module.exports = () => {
 	return {
 		mode: 'development',
@@ -48,7 +46,7 @@ module.exports = () => {
 				],
 			}),
 		],
-
+		// CSS loaders and babel to webpack.
 		module: {
 			rules: [
 				{
@@ -58,7 +56,7 @@ module.exports = () => {
 				{
 					test: /\.m?js$/,
 					exclude: /node_modules/,
-					// We use babel-loader in order to use ES6.
+					// use babel-loader in order to use ES6.
 					use: {
 						loader: 'babel-loader',
 						options: {
